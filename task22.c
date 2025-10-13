5338afb83441ca14a869aeacb0d667ffd155f411
Давид Эксузян ТКИ-142 <dobrau.07@gmail.com>
2025-10-13 12:37:11 +0300

    Create task22.c

---
+++ task22.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#define _USE_MATH_DEFINES
+#include<math.h>
+
+/**
+* @brief
+* @param a - константа а
+* @param x - переменная х
+* @return - возвращает значение функции
+*/
+
+double gety1(double x, double a);
+double gety2(double x, double a);
+
+/**
+* @brief Точка входа в программу
+* @return Возваращет 0, если программа была выполнена корректно, иначе 1
+*/
+
+int main(void)
+{
+	system("chcp 1251");
+	const double a = 1.36;
+	double x = 0;
+	printf("Введите значение переменной x: ");
+	scanf_s("%lf", &x);
+	if (x < 0.5)
+	{
+		printf("Значение функции при x<0.5: %.2f",gety1(x, a));
+	}
+	if (x >= 0.5)
+	{
+		printf("Значение функции при x>=0.5: %.2f", gety2(x, a));
+	}
+	return 0;
+}
+double gety1(double x,double a)
+{
+	return M_PI / 2 + x * a - exp(a * x);
+}
+double gety2(double x, double a)
+{
+	return pow(x, 3) * a + 28;
+}
