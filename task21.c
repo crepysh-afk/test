9572bdc5349a3c49cb718870fd0d28b2c5ee59bd
Давид Эксузян ТКИ-142 <dobrau.07@gmail.com>
2025-10-27 12:28:40 +0300

    Update task21.c

--- task21.c
+++ task21.c
@@ -9,7 +9,36 @@
  * @param k Указатель на переменную для записи количества отрицательных чисел
  * @return Абсолютное значение суммы a, b, c
  */
-double analyze_numbers(double a, double b, double c, int* k) 
+double analyze_numbers(double a, double b, double c, int* k);
+
+double getValue();
+
+/**
+* @brief Точка входа в программу
+* @return Возваращет 0, если программа была выполнена корректно, иначе 1
+*/
+int main()
+{
+	system("chcp 1251");
+	double abs_sum = 0;
+	int k = 0;
+
+	printf("Введите переменную а: ");
+	double a = getValue();
+	printf("Введите переменную b: ");
+	double b = getValue();
+
+	printf("Введите переменную c: ");
+	double c = getValue();
+	abs_sum = analyze_numbers(a, b, c, &k);
+
+	printf("Количество отрицательных чисел: %d\n ", k);
+	printf("Абсолютное значение суммы чисел: %.2f\n ", abs_sum);
+
+	return 0;
+}
+
+double analyze_numbers(double a, double b, double c, int* k)
 {
 	*k = 0;
 	if (a < 0) (*k)++;
@@ -19,37 +48,13 @@ double analyze_numbers(double a, double b, double c, int* k)
 	double sum = a + b + c;
 	return abs(sum);
 }
-
-
-/**
-* @brief Точка входа в программу
-* @return Возваращет 0, если программа была выполнена корректно, иначе 1
-*/
-
-
-int main()
+double getValue()
 {
-	system("chcp 1251");
-	double a;
-	double b;
-	double c;
-	double abs_sum = 0;
-	int k = 0;
-
-	printf("Введите переменную а: ");
-	scanf_s("%lf", &a);
-
-	printf("Введите переменную b: ");
-	scanf_s("%lf", &b);
-
-	printf("Введите переменную c: ");
-	scanf_s("%lf", &c);
-
-	abs_sum = analyze_numbers(a, b, c, &k);
-	
-	printf("Количество отрицательных чисел: %d\n ", k);
-	printf("Абсолютное значение суммы чисел: %.2f\n ", abs_sum);
-
-	return 0;
+	double value = 0;
+	if (!scanf_s("%lf", &value))
+	{
+		printf("Неверный ввод!");
+		abort();
+	}
+	return value;
 }
-
